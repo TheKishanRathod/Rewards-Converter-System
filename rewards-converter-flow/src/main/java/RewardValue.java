4481d79c784bbe2e03b864f291e44fbc4b81a7bc
TheKishanRathod <125811262+TheKishanRathod@users.noreply.github.com>
2024-05-10 14:23:48 +0530

    Add files via upload

---
+++ rewards-converter-flow/src/main/java/RewardValue.java
@@ -0,0 +1,30 @@
+public class RewardValue {
+    private double cashValue;
+    private double milesValue;
+
+    // Constructor accepting cash value
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        // Convert cash value to miles using the conversion rate
+        this.milesValue = cashValue / 0.0035;
+    }
+
+    // Constructor accepting miles value
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            // Convert miles to cash using the conversion rate
+            this.cashValue = milesValue * 0.0035;
+        }
+    }
+
+    // Get cash value
+    public double getCashValue() {
+        return cashValue;
+    }
+
+    // Get miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
+}
